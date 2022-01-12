beb0c91c5fceb9b5b15a44ce2f5816408389e8ce
VanessaTrajano <vanessapalmeirakelmertrajano@gmail.com>
2022-01-12 18:06:03 -0300

    Subindo a Dinâmica da primeira aula de lógica :)

---
+++ script.js
@@ -0,0 +1,32 @@
+// Crie uma variável que receba um nome;
+
+let nome = `Vanessa`
+console.log(nome)
+
+// Crie uma variável que receba um número;
+
+let number = 4
+console.log(number)
+
+// Crie uma variável com o ano atual e subtraia pelo número que você guardou;
+
+let resultado = 2022 - number
+console.log(resultado)
+
+// Agora mostre no console o resultado da soma das duas variáveis guardadas anteriormente;
+
+console.log(resultado + number)
+
+//Mostre no console a mensagem "Estou mexendo no console.log";
+
+console.log("estou mexendo no console.log")
+
+// Crie uma variável chamada “quartaFeira” e atribua à ela o valor booleano que representa verdadeiro ou falso e mostre a saída no console;
+
+let quartaFeira = true
+console.log(quartaFeira)
+
+// Exiba no console o tipo de dado da váriavel quartaFeira;
+
+let tipoDado = `Boolean`
+console.log(tipoDado)
